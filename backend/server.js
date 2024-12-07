ac3549b94e50a2d055651a4b0d78df11d4e2c85b
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-12-07 12:29:51 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"; // Import the router
-import connectMongoDB from "../db/connectMongoDB.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 const app = express();
