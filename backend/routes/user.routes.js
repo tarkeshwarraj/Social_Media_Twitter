c09e830068d6bd5ccbc75d5ea65e7a1ce5576bde
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-12-10 14:49:29 +0530

    user routs and controllers

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { getUserProfile, followUnfollowUser} from '../controllers/user.controller.js';
+import { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUser} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get("/profile/:username",protectRoute, getUserProfile);
-// router.get("/suggested",protectRoute, getUserProfile);
+router.get("/suggested",protectRoute, getSuggestedUsers);
 router.post("/follow/:id",protectRoute, followUnfollowUser);
-// router.post("/update",protectRoute, updateUserProfile);
+router.post("/update",protectRoute, updateUser);
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,16 @@ import authRoutes from "./routes/auth.routes.js"; // Import the router
 import userRoutes from "./routes/user.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 console.log(process.env.MONGO_URI)
