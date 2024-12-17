495ba33d704fcef8087fea8dc3cfa4473d0fb26c
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-12-17 18:07:17 +0530

    post and like controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,23 @@
 export const commentOnPost = async(req, res) => {
 
 };
+
+export const likeUnlikePost = async(req, res) =>{
+
+};
+
+export const getAllPosts = async (req, res) => {
+    try{
+        const posts = await Post.find().sort({ createdAt: -1});
+
+        if(posts.length === 0){
+            return res.status(200).json([])
+        }
+
+        res.status(200).json(posts);
+
+    }catch(error){
+        console.log("Error in getAllPosts controller: ", error);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
