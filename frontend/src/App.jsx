d79c4d74d292a50f64999ca26bd7ee19ff50c5e7
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-12-31 18:28:09 +0530

    frontend1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
    <>
-   <p className='text-5xl text-red-500'>Hello from App.jsx thanks you part2</p>
+   <p className='text-5xl text-red-500'>Hello from Ap.jsx thanks you part2</p>
    </>
   )
 }
