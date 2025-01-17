117b13ad48fad3e865ee5c1e21d361aee9f1c281
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-01-17 19:03:33 +0530

    daisy UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <>
-   <p className='text-5xl text-red-500'>Hello rakfrom Apjsx thanks you part2</p>
+   <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
    </>
   )
 }
 
 export default App
-
-
-
-fracov004
-
-
-81.89
-89.64
