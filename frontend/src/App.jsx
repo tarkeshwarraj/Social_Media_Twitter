8044b8813c4f0f3028ce951c4ebd790064de786d
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-01-20 19:52:45 +0530

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,18 @@ import './App.css'
 import HomePage from './pages/home/HomePage'
 import SignUpPage from './pages/auth/signup/SignUpPage'
 import LoginPage from './pages/auth/login/LoginPage'
+import Sidebar from './components/common/Sidebar'
+import NotificationPage from './pages/notification/NotificationPage'
 
 function App() {
 	return (
 		<div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignUpPage />} />
 				<Route path='/login' element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
 			</Routes>
 		</div>
 	);
